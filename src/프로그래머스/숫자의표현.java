626b849704d179cfe93c09a8e368bdcbdb0c26ba
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-09-10 08:44:35 +0900

    연습문제 :: 숫자의 표현

---
+++ src/프로그래머스/숫자의표현.java
@@ -0,0 +1,32 @@
+package 프로그래머스;
+
+public class 숫자의표현 {
+    public static void main(String[] args) {
+        System.out.println("solution(15) = " + solution(15));
+    }
+
+    public static int solution(int n) {
+        int cnt = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int sum = i;
+
+            if (i + 1 > n) {
+                break;
+            }
+            if (i == n / 2 + 1) break;
+
+            for (int j = i + 1; j <= n; j++) {
+                sum += j;
+
+                if (sum == n) {
+                    cnt++;
+                    break;
+                }
+
+            }
+        }
+
+        return cnt;
+    }
+}
